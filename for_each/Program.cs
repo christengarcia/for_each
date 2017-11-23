8edd71407c91b0d628678548affb44bcbc9499e4
Christen Garcia <30610163+christengarcia@users.noreply.github.com>
2017-11-23 21:35:17 +1100

    First commit

---
+++ for_each/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace for_each
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] message = { 'H', 'e', 'l', 'l', 'o' };
+
+            // A foreach loop is very useful if you want 
+            // to get information from an array or list,
+            // without making any changes to it.
+            foreach (char i in message)
+                Console.Write(i);
+        }
+    }
+}
